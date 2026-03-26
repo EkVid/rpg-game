13286c884f4e41377545dc17c4ffabd159fb9601
Austin <austinjas014@gmail.com>
2026-03-25 21:47:14 -0400

    basic gameplay

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -5,11 +5,38 @@ public partial class Player : CharacterBody2D
 {
 	private const float Speed = 300.0f;
 	private Vector2 direction;
+	private AnimatedSprite2D character;
+	private float x;
+	private float y;
+	private bool isAttacking = false;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		character = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		
+		character.AnimationFinished += () =>
+		{
+			if (character.Animation == "attack")
+			{
+				isAttacking = false;
+			}
+		};
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		float x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-		float y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+		HandleAttack();
+		HandleMovement();
+		HandleFlip();
+		UpdateAnimation();
+		MoveAndSlide();
+	}
+
+	private void HandleMovement()
+	{
+		x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+		y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 		
 		direction = new Vector2(x,y);
 
@@ -21,7 +48,34 @@ public partial class Player : CharacterBody2D
 		{
 			Velocity = Vector2.Zero;
 		}
-		
-		MoveAndSlide();
+	}
+
+	private void HandleFlip()
+	{
+		if(x != 0) 
+			character.FlipH = x < 0;
+	}
+
+	private void HandleAttack()
+	{
+		if (Input.IsActionJustPressed("attack") &&  !isAttacking)
+		{
+			isAttacking = true;
+			PlayAnimation("attack");
+		}
+	}
+
+	private void UpdateAnimation()
+	{
+		if (isAttacking)
+			return;
+		string anima = direction != Vector2.Zero ? "run" : "idle";
+		PlayAnimation(anima);
+	}
+
+	private void PlayAnimation(String animation)
+	{
+		if(character.Animation != animation) 
+			character.Play(animation);
 	}
 }
